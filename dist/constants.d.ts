ab42f531f7060597f87d430de0e50ca5cf74151d
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-10-19 07:07:08 +0000

    Add dist

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0xC0FBc684cACa3f37Adc046286401c76adceA2eD9";
+export declare const INIT_CODE_HASH = "0x0ccaea73dba641b0fe2f63a21789a4cd3bf852caf7e4462980af7d25f613e588";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
