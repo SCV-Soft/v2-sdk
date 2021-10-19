ead9c0f16f107d9a68ada0c87f14285f0f40e6a5
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-10-19 06:59:36 +0000

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xC0FBc684cACa3f37Adc046286401c76adceA2eD9'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x0ccaea73dba641b0fe2f63a21789a4cd3bf852caf7e4462980af7d25f613e588'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
